f656eebf5d867b1583fea2ebb1450f68a07d3950
fenshitianyue <abzanda@163.com>
2020-02-06 02:06:23 +0800

    update code

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ def check_data_source(table_name):
         return elastic
 
 def search(query):
-    raw_search_result_list = []  # [{"join_type": xxx, "data": xxx, "on": xxx, "export": xxx}, ...]
+    raw_search_result_list = []  # [{"join_type": xxx, "data": DataFrame, "on": xxx, "export": xxx}, ...]
     # 收集主语法块查询结果
     main_query_item = {
         "query": {
@@ -28,16 +28,15 @@ def search(query):
     }
     raw_search_result_list.append(check_data_source(query["select"]["from"]).search(main_query_item))
     # 收集 join 语法块所有查询结果
-    for table in query['join']:
-        raw_search_result_list.append(check_data_source(table["query"]["select"]["from"]).search(query["select"]))
+    for join_item in query['join']:
+        raw_search_result_list.append(check_data_source(join_item["query"]["select"]["from"]).search(query["select"]))
 
     # 在内存中join
-    df_main = ""
+    dataframe_main = ""
     if query["join"]:
         for item in raw_search_result_list:
-            if not item.exists():
+            if not item["data"].index:
                 continue
-            item["data"] = pd.DataFrame.from_records(list(item["data"]))
             alias_map = {}
             for field in item["export"]:
                 if field.find("@") != -1:
@@ -45,17 +44,17 @@ def search(query):
             item["data"] = item["data"].rename(columns=alias_map)
             # TODO: 打印单表结果
             if "left_join" == item["join_type"]:
-                df_main = df_main.merge(item["data"], on=item["on"], how="left")
+                dataframe_main = dataframe_main.merge(item["data"], on=item["on"], how="left")
             elif "inner_join" == item["join_type"]:
-                df_main = df_main.merge(item["data"], on=item["on"])
+                dataframe_main = dataframe_main.merge(item["data"], on=item["on"])
             elif "full_join" == item["join_type"]:
-                df_main = df_main.merge(item["data"], on=item["on"], how="outer")
+                dataframe_main = dataframe_main.merge(item["data"], on=item["on"], how="outer")
             else:
-                pass
+                pass  # TODO: 抛出 restsql 语法错误
     # TODO: 打印 join 结果
 
     # 封装 grafana 预处理数据集
-    if len(df_main) == 0:
+    if len(dataframe_main) == 0:
         return [], []
 
 
