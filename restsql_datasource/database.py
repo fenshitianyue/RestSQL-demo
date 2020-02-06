65cea8e8dcb3e478256a1f29f51831b81efcc716
fenshitianyue <abzanda@163.com>
2020-02-06 12:55:52 +0800

    remove code

---
+++ restsql_datasource/database.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import django.db.models
+import pandas as pd
+import model_set
+# from init_table_map import *
+from init_table_map import table_map
+
+DEFAULT_LIMIT = 1000
+
+def get_model(model_name):
+    return getattr(model_set, table_map[model_name])
+
+def get_attribute(attribute_name):
+    return getattr(django.db.models, attribute_name)
+
+def process_table_name(raw_table_name):
+    """函数作用：
+    兼容已有的以sngapm开头的表，对restsql中的表名做处理:
+    schema_name.table_name -> "schema_name"."table_name"
+    """
+    pos = raw_table_name.find(".")
+    if pos != -1:
+        schema, table_name = raw_table_name.split(".")[0], raw_table_name.split(".")[1]
+        return '"{}"."{}"'.format(schema, table_name)
+    else:
+        return raw_table_name
+
+def get_table_limit(raw_limit):
+    if raw_limit is None or raw_limit > DEFAULT_LIMIT:
+        return DEFAULT_LIMIT
+    return raw_limit
+
+def build_aggregation_dict(raw_aggregation):
+    agg_dict = {}
+    for it in raw_aggregation:
+        field = it[:it.find("__")]
+        if it.find("__count") != -1:
+            if it.find("_distinct") != -1:
+                agg_dict.update({it: get_attribute("Count")(field, distinct=True)})
+            else:
+                agg_dict.update({it: get_attribute("Count")(field)})
+        if it.find("__sum") != -1:
+            if it.find("_distinct") != -1:
+                agg_dict.update({it: get_attribute("Sum")(field, distinct=True)})
+            else:
+                agg_dict.update({it: get_attribute("Sum")(field)})
+        if it.find("__avg") != -1:
+            if it.find("_distinct") != -1:
+                agg_dict.update({it: get_attribute("Avg")(field, distinct=True)})
+            else:
+                agg_dict.update({it: get_attribute("Avg")(field)})
+    return agg_dict
+
+def search_impl(table, select, agg_dict, table_limit, search_option):
+    if search_option["has_filter"] and search_option["has_groupby"]:
+        result = table.objects.values(*select["group_by"]).filter(**select["filter"]).annotate(**agg_dict)
+    elif not search_option["has_filter"] and search_option["has_groupby"]:
+        result = table.objects.values(*select["group_by"]).annotate(**agg_dict)
+    elif search_option["has_filter"] and not search_option["has_groupby"]:
+        for it in select["fields"]:
+            if it.find("__avg") != -1 or it.find("__sum") != -1 or it.find("__count") != -1:
+                pass  # 抛出 restsql 语法错误
+        result = table.objects.filter(**select["filter"]).values(*select["fields"])
+    else:
+        for it in select["fields"]:
+            if it.find("__avg") != -1 or it.find("__sum") != -1 or it.find("__count") != -1:
+                pass  # 抛出 restsql 语法错误
+        result = table.objects.values(*select["fields"])
+    if select.get("sort"):
+        return result.order_by(*select["sort"])[:table_limit]
+    else:
+        return result[:table_limit]
+
+def package_result(query_item, result):
+    result = {
+        "data": result,
+        "on": [],
+        "export": []
+    }
+    if query_item.get("type"):
+        result["type"] = query_item["type"]
+        result["on"] = query_item["on"].keys()
+        # 这里的逻辑是: join块中的fields字段的元素数目减去on字段的key个数必须等于export字段的元素个数
+        diff = len(query_item['query']['select']['fields']) - len(query_item["on"]) - len(query_item['export'])
+        if diff > 0:
+            raise SyntaxError('The elements in the "fields" field of the "join" query contain unused columns')
+        elif diff < 0:
+            raise SyntaxError('Check your "join" syntax')
+        # 收集export字段
+        result["export"].append(query_item['export'])
+    # return {"data": raw_result}
+    return result
+
+def convert_result_to_dataframe(result):
+    return pd.DataFrame.from_records(list(result))
+
+def search(query_item):
+    """return
+    [{"join_type": xxx, "data": DataFrame, "on": xxx, "export": xxx}]
+    """
+    select = query_item["query"]["select"]
+    table = get_model(process_table_name(select["from"]))
+    search_option = {
+        "has_filter": False,
+        "has_groupby": False
+    }
+    if select.get("filter"):
+        search_option["has_filter"] = True
+    if select.get("group_by"):
+        search_option['has_groupby'] = True
+        if select.get("aggregation"):
+            agg_dict = build_aggregation_dict(select["aggregation"])
+        else:
+            pass  # 抛出 restsql 语法错误
+    table_limit = get_table_limit(select.get("limit"))
+    result = search_impl(table, select, agg_dict, table_limit, search_option)
+    result_dataframe = convert_result_to_dataframe(result)
+    # TODO: 打印此时生成的 sql
+    return package_result(query_item, result)
+
---
+++ restsql_datasource/elastic.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from elasticsearch import Elasticsearch
+from sngapm_web.settings import ES_CONN
+import pandas as pd
+
+DEFAULT_LIMIT = 1000
+
+def get_table_limit(raw_limit):
+    if raw_limit is None or raw_limit > DEFAULT_LIMIT:
+        return DEFAULT_LIMIT
+    return raw_limit
+
+def parse_restsql_to_dsl(select_syntax_item):
+    if select_syntax_item["from"] is None or select_syntax_item["from"] == "":
+        pass  # 抛出 restsql 语法错误
+
+    dsl = {
+        'size': get_table_limit(select_syntax_item.get("limit")),
+        'query': {
+            'bool': {
+                'must': []
+            }
+        },
+        'sort': [],
+        '_source': {
+            'includes': []
+        },
+        'aggs': {
+            'groupby': {
+                'terms': {
+                    'script': {
+                        'source': ''
+                    }
+                },
+                'aggs': {}
+            }
+        },
+        'from': select_syntax_item['from']
+    }
+    dsl['_source']['includes'] = select_syntax_item['fields']
+    dsl_where = dsl['query']['bool']['must']
+    dsl_group_by = ''
+    dsl_aggs = dsl['aggs']['groupby']['aggs']
+    dsl_sort = dsl['sort']
+
+    # 处理filter
+    for field, value in select_syntax_item['filter'].items():
+        if '__' not in field:
+            dsl_where.append({
+                'term': {
+                    field: value
+                }
+            })
+        else:
+            op = field.split('__')[1]
+            field_name = field.split('__')[0]
+            if op == 'gt':
+                dsl_where.append({
+                    'range': {
+                        field_name: {'gt': value}
+                    }
+                })
+            elif op == 'lt':
+                dsl_where.append({
+                    'range': {
+                        field_name: {'lt': value}
+                    }
+                })
+            elif op == 'gte':
+                dsl_where.append({
+                    'range': {
+                        field_name: {'gte': value}
+                    }
+                })
+            elif op == 'lte':
+                dsl_where.append({
+                    'range': {
+                        field_name: {'lte': value}
+                    }
+                })
+            elif op == 'contains':
+                """"
+                TODO: 本来想用match/match_phrase来进行模糊匹配，但是由于这两种查询由于分词的缘故，现有的
+                      分词情况并不能完美的模拟sql中的like，所以暂时采用正则查询。正则查询的效率很低。
+                dsl_where.append({
+                    'match_phrase': {
+                        field_name: {
+                            'query': value
+                        }
+                    }
+                })
+                """
+                dsl_where.append({
+                    'wildcard': {field_name: ''.join(['*', value, '*'])}
+                })
+            elif op == 'startswith':
+                dsl_where.append({
+                    'prefix': {field_name: value}
+                })
+            elif op == 'endswith':
+                dsl_where.append({
+                    'wildcard': {field_name: ''.join(['*', value])}
+                })
+            elif op == 'range':
+                if len(value) != 2:
+                    raise SyntaxError('Check your "range" query')
+                dsl_where.append({
+                    'range': {
+                        field_name: {'gte': value[0], 'lte': value[1]}
+                    }
+                })
+            elif op == 'in':
+                dsl_where.append({
+                    'terms': {field_name: value}
+                })
+            else:
+                raise SyntaxError('cat not support op: {0}, field: {1}'.format(op, field))
+    if select_syntax_item.get('group_by'):
+        # 处理 group by
+        """
+        由于ES 6.x以下版本不支持 composite 语法，所以这里采用script方式来实现group by，用来兼容不同版本ES这部分语法的差异性
+        script中source的格式：key:value;key:value
+        定义成这个样子是方便后面从查询结果中提取数据
+        """
+        for field in select_syntax_item['group_by']:
+            dsl_group_by = ''.join([dsl_group_by, "'", field, "'", " + ':' + ", "doc['", field, "'].value", " + ';' + "])
+        dsl_group_by = dsl_group_by[:len(dsl_group_by)-len(" + ';' + ")]  # 去掉结尾的 " + ';' + "
+        dsl['aggs']['groupby']['terms']['script']['source'] = dsl_group_by
+        # 处理 aggregation
+        for field in select_syntax_item['aggregation']:
+            field_name, op = field.split('__')[0], field.split('__')[1]
+            func_map = {'count': 'value_count', 'sum': 'sum', 'avg': 'avg', 'max': 'max', 'min': 'min', 'count_distinct': 'cardinality'}
+            if op in func_map:
+                dsl_aggs[field] = {func_map[op]: {'field': field_name}}
+            else:
+                raise SyntaxError('cat not support aggregation operation: {}'.format(op))
+    else:
+        del dsl['aggs']
+
+    # 处理 sort
+    if select_syntax_item.get('sort'):
+        for sort_it in select_syntax_item['sort']:
+            is_reverse = sort_it.find('-')
+            if is_reverse != 0:
+                dsl_sort.append({
+                    sort_it: {'order': 'asc'}
+                })
+            else:
+                field = ''.join(sort_it.split('-')[1:])
+                dsl_sort.append({
+                    field: {'order': 'desc'}
+                })
+    else:
+        del dsl['sort']
+    # TODO: 打印此时生成的 DSL
+    return dsl
+
+def extract_useful_result(raw_result):
+    if 'aggs' in raw_result or 'aggregations' in raw_result:
+        if raw_result.get('aggregations'):
+            result = raw_result['aggregations']['groupby']['buckets']
+        else:
+            result = raw_result['agg']['groupby']['buckets']
+        for it in result:
+            pair = it['key'].split(';')
+            for pair_item in pair:
+                it.update({pair_item.split(':')[0]: pair_item.split(':')[1]})
+            del it['key']
+            del it['doc_count']  # TODO: 暂时没用的一个字段
+            for key, value in it.items():
+                if isinstance(value, dict) and 'value' in value:
+                    it[key] = value['value']
+    elif 'hits' in raw_result and 'hits' in raw_result['hits']:
+        result = list(map(lambda x: x['_source'], raw_result['hits']['hits']))
+    return result
+
+def search_impl(dsl):
+    es_client = Elasticsearch(ES_CONN)
+    index = dsl["from"]
+    del dsl["from"]
+    return extract_useful_result(es_client.search(index=index, body=dsl))
+
+def convert_result_to_dataframe(result, fields):
+    data = []
+    for item in result:
+        line = []
+        for field in fields:
+            line.append(item[field])
+        data.append(line)
+    result_dataframe = pd.DataFrame(data, columns=fields)
+    return result_dataframe
+
+def package_result(query_item, result):
+    result = {
+        "data": result,
+        "on": [],
+        "export": []
+    }
+    if query_item.get("type"):
+        result["type"] = query_item["type"]
+        result["on"] = query_item["on"].keys()
+        # 这里的逻辑是: join块中的fields字段的元素数目减去on字段的key个数必须等于export字段的元素个数
+        diff = len(query_item['query']['select']['fields']) - len(query_item["on"]) - len(query_item['export'])
+        if diff > 0:
+            raise SyntaxError('The elements in the "fields" field of the "join" query contain unused columns')
+        elif diff < 0:
+            raise SyntaxError('Check your "join" syntax')
+        # 收集export字段
+        result["export"].append(query_item['export'])
+    # return {"data": raw_result}
+    return result
+
+def search(query_item):
+    """return
+    [{"join_type": xxx, "data": DataFrame, "on": xxx, "export": xxx}]
+    """
+    select = query_item["query"]["select"]
+    dsl = parse_restsql_to_dsl(select)
+    result = search_impl(dsl)
+    result_dataframe = convert_result_to_dataframe(result, select["fields"])
+    return package_result(query_item, result_dataframe)
+
