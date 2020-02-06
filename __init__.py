589d3e85b92af613066c2098ebc5e4e15e98142c
fenshitianyue <abzanda@163.com>
2020-02-06 15:05:42 +0800

    基本完成 restsql 的主体框架

--- __init__.py
+++ __init__.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
+import json
 from restsql_datasource import database
 from restsql_datasource import elastic
+import restsql_utility
 
 data_source_map = {
     "database": database,
@@ -17,6 +19,33 @@ def check_data_source(table_name):
         return data_source_map[table_prefix]
     except KeyError:
         return elastic
+
+def sort_result(dataframe, query):
+    sort_methods = []
+    for field in query["sort"]:
+        if field.find("-") != -1:
+            field = field.split("-")[0]
+            sort_methods.append(False)
+        else:
+            sort_methods.append(True)
+    dataframe.sort_values(by=query["sort"], ascending=sort_methods)
+    return dataframe
+
+def package_query_data(dataframe, alias_map):
+    """函数作用:
+    按照一定的格式将pandas结果集存储到python的数据结构中
+    """
+    data_list = json.loads(dataframe.to_json(orient='records'))
+
+    for line in data_list:
+        # key 是原列名，value 是列别名
+        for key, value in alias_map.items():
+            if value in line.keys():
+                continue
+            line.update({value: line[key]})  # 添加 {列别名: 值} 的键值对
+            del line[key]  # 删除 {原列名: 值} 的键值对
+
+    return data_list
 
 def search(query):
     raw_search_result_list = []  # [{"join_type": xxx, "data": DataFrame, "on": xxx, "export": xxx}, ...]
@@ -51,10 +80,26 @@ def search(query):
                 dataframe_main = dataframe_main.merge(item["data"], on=item["on"], how="outer")
             else:
                 pass  # TODO: 抛出 restsql 语法错误
+    # if len(dataframe_main) == 0:
+    if not dataframe_main.index:
+        return [], []
     # TODO: 打印 join 结果
+    dataframe_main = sort_result(dataframe_main, query)
+    dataframe_main = dataframe_main[0:restsql_utility.get_table_limit(query.get("limit"))]
+
+    dataframe_main = dataframe_main.fillna("null")
 
     # 封装 grafana 预处理数据集
-    if len(dataframe_main) == 0:
-        return [], []
+    alias_map = {}
+    for field in query['fields']:
+        if field.find('@') != -1:
+            alias_map.update({field.split('@')[0]: field.split('@')[1]})
 
+    # 根据顶级字段fields中的值对查询结果集进行过滤，去掉顶级字段fields中不存在的列
+    need_filter_columns = list(set(dataframe_main.columns).difference(set(alias_map.keys())))
+    df_main = dataframe_main.drop(need_filter_columns, axis='columns')
+
+    data_list = package_query_data(df_main, alias_map)
+    field_list = alias_map.values()
+    return data_list, field_list
 
